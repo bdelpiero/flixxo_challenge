56942787c939de558923aa4f6ae803715e68ab72
Bruno Del Piero <bdelpiero93@gmail.com>
2022-12-30 18:20:16 -0300

    project setup

---
+++ src/data-source.ts
@@ -0,0 +1,16 @@
+import { DataSource } from "typeorm"
+
+// TODO: fix port type
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: Number(process?.env?.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ["src/entity/*.ts"],
+  migrations: ["src/migration/*.ts"],
+  synchronize: true,
+  logging: true,
+  subscribers: [],
+})
